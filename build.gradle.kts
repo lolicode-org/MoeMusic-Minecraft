cbfbac7076f74fd0c89ac5aca6856e5e19bd9803
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2026-05-24 11:48:32 +0800

    build: remove android x86/x86_64/arm32 and linux arm32 for smaller size

--- build.gradle.kts
+++ build.gradle.kts
@@ -183,6 +183,12 @@ val buildNeoForgeFullJar by tasks.registering(ShadowJar::class) {
         "**/lavaplayer/source/vimeo/**",
         "**/lavaplayer/source/yamusic/**",
         "**/lavaplayer/source/youtube/**",
+        "natives/android-armhf/**",
+        "natives/android-x86/**",
+        "natives/android-x86-64/**",
+        "natives/linux-aarch32/**",
+        "natives/linux-arm/**",
+        "natives/linux-armhf/**",
     )
 
     listOf(
@@ -251,6 +257,12 @@ val buildForgeFullJar by tasks.registering(ShadowJar::class) {
         "**/lavaplayer/source/vimeo/**",
         "**/lavaplayer/source/yamusic/**",
         "**/lavaplayer/source/youtube/**",
+        "natives/android-armhf/**",
+        "natives/android-x86/**",
+        "natives/android-x86-64/**",
+        "natives/linux-aarch32/**",
+        "natives/linux-arm/**",
+        "natives/linux-armhf/**",
     )
 
     listOf(
