6c264183c947c7e064d3b14a293b7806788c1ab0
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2026-08-16 17:15:44 +0000

    feat: clear queue command and button

--- platform-common/src/client/kotlin/org/lolicode/moemusic/platform/client/network/ClientNetworkSetup.kt
+++ platform-common/src/client/kotlin/org/lolicode/moemusic/platform/client/network/ClientNetworkSetup.kt
@@ -57,6 +57,7 @@ object ClientNetworkSetup {
             PacketIds.QUEUE_RESPONSE,
             PacketIds.UI_BOOTSTRAP_RESPONSE,
             PacketIds.QUEUE_REMOVE_RESPONSE,
+            PacketIds.QUEUE_CLEAR_RESPONSE,
             PacketIds.PLAYBACK_CONTROL_RESPONSE,
             PacketIds.CONTENT_FILTER_ACTION_RESPONSE,
         ).forEach { packetId ->
