930a6e616fc6da0aa0bca8b06da06a47142a3501
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2026-09-14 23:41:08 +0000

    refactor(client): separate issue presenter and connection coordinator from shortcut controller

--- forge/src/main/kotlin/org/lolicode/moemusic/client/MoeMusicClient.kt
+++ forge/src/main/kotlin/org/lolicode/moemusic/client/MoeMusicClient.kt
@@ -14,6 +14,8 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import net.minecraftforge.fml.loading.FMLPaths
 import org.lolicode.moemusic.MoeMusic
 import org.lolicode.moemusic.MoeMusicForgeBuildInfo
+import org.lolicode.moemusic.platform.client.bootstrap.ClientConnectionCoordinator
+import org.lolicode.moemusic.platform.client.bootstrap.ClientPluginIssuePresenter
 import org.lolicode.moemusic.platform.client.bootstrap.ClientRuntimeBootstrap
 import org.lolicode.moemusic.platform.client.bootstrap.ClientShortcutController
 import org.lolicode.moemusic.platform.client.bootstrap.MoeMusicClientKeyBindingRegistry
@@ -114,16 +116,18 @@ object MoeMusicClient {
 
     private fun onClientLoggedIn(event: ClientPlayerNetworkEvent.LoggedInEvent) {
         val bindings = keyBindings ?: return
-        ClientShortcutController.onConnectionJoined(Minecraft.getInstance(), bindings)
+        ClientConnectionCoordinator.onConnectionJoined(Minecraft.getInstance(), bindings.openGuiKey)
     }
 
     private fun onClientLoggedOut(event: ClientPlayerNetworkEvent.LoggedOutEvent) {
-        ClientShortcutController.onConnectionDisconnected()
+        ClientConnectionCoordinator.onConnectionDisconnected()
     }
 
     private fun onClientTick(event: TickEvent.ClientTickEvent) {
         if (event.phase != TickEvent.Phase.END) return
+        val mc = Minecraft.getInstance()
+        ClientPluginIssuePresenter.handleClientTick(mc)
         val bindings = keyBindings ?: return
-        ClientShortcutController.handleEndClientTick(Minecraft.getInstance(), bindings)
+        ClientShortcutController.handleEndClientTick(mc, bindings)
     }
 }
