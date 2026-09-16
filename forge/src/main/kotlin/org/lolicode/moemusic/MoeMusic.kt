7d0170403b44026704dc037f9fb5ac542d7bcd40
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2026-09-16 22:10:10 +0000

    fix: add logs for more user actions & remove dup logging
    also drop/move to debug level for some logs that are not useful
    for daily administration or may be vulnerable to dos attack

--- forge/src/main/kotlin/org/lolicode/moemusic/MoeMusic.kt
+++ forge/src/main/kotlin/org/lolicode/moemusic/MoeMusic.kt
@@ -55,7 +55,11 @@ object MoeMusic {
     }
 
     private fun onServerStopping(event: ServerStoppingEvent) {
-        MoePlatform.serverShutdown(finalRuntime = event.server.isDedicatedServer)
+        runCatching {
+            MoePlatform.serverShutdown(finalRuntime = event.server.isDedicatedServer)
+        }.onFailure {
+            logger.error("Failed to shut down MoeMusic", it)
+        }
     }
 
     private fun onRegisterCommands(event: RegisterCommandsEvent) {
